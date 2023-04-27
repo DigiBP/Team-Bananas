1206e66ad6edb30854fecd3b122c67530a85d407
Dietrich Rordorf <drordorf@gmail.com>
2023-04-27 23:50:16 +0200

    add employee data sample, add data init method to populate vector database

--- app/frontend/server-middleware/store/api.js
+++ app/frontend/server-middleware/store/api.js
@@ -13,7 +13,7 @@ const client = weaviate.client({
   host
 })
 
-// axios to make requests against Camunda REST Engine API
+// axios to make requests against Weaviate API
 // const axios = require('axios')
 
 /* API endpoints */
@@ -49,5 +49,41 @@ app.all('/init', (req, res) => {
 
   return res.send('OK')
 })
+
+// API endpoint 1: init data
+app.all('/init-data', (req, res) => {
+  // create Employee profiles
+  const data = require('./employees.json')
+
+  data.forEach((employee) => {
+    client.data
+      .creator()
+      .withClassName('Employee')
+      .withId(generateUUID())
+      .withProperties({
+        name: employee.name,
+        age: employee.age,
+        position: employee.position,
+        degree: employee.degree,
+        level: employee.level,
+        experience: employee.experience
+      })
+      .do()
+      .then((res) => {
+        console.log(res) // eslint-disable-line no-console
+      })
+      .catch((err) => {
+        console.error(err) // eslint-disable-line no-console
+      })
+  })
+})
+
+function generateUUID () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
 
 module.exports = app
