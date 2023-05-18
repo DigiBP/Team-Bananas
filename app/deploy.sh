0e505ff143d8734d2d8465deed5b60a6f9b4f4c7
Dietrich Rordorf <drordorf@gmail.com>
2023-05-18 19:42:12 +0200

    add logs follow to the deploy script

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# get latest code changes
 git pull
+
+# turn off the stack and remove old container versions
 docker compose -f ./docker-compose.prod.yml down
 docker rmi app-node
 docker rmi app-automation
+
+# rebuild new container and start stack
 docker compose -f ./docker-compose.prod.yml up -d
+
+# follow the stack logs
+docker compose logs -f
